ca5b26116493d295ea4fe66517459050e1953d1d
manojfulumbarkar <100022849+manojfulumbarkar@users.noreply.github.com>
2024-05-08 15:27:03 +0530

    Update install_jenkins.sh

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -31,7 +31,8 @@ sudo apt-get install trivy -y
 sudo apt install wget -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+sudo apt update
+sudo snap install terraform --classic
 
 #install Kubectl on Jenkins
 sudo apt update
@@ -45,4 +46,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
+
 
